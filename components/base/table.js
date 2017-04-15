55cce1e6e52d69f45b82b333c25199db542256cc
Kondratenko Pavel <p.kondratenko@rambler-co.ru>
2017-04-15 17:09:04 +0300

    change percents calculation logic, add delimiter for progress bars

--- components/base/table.js
+++ components/base/table.js
@@ -4,11 +4,15 @@ const chalk = require('chalk');
 const Base = require('./base');
 const {merge, padLeft, padRight} = require('./../../helpers');
 
+const CHAR_DELIMITER = '|';
+const CHAR_EMPTY = '░';
+const CHAR_FILL = '█';
+
 module.exports = class extends Base {
 
   constructor(options) {
     super(merge({
-      progressSize: 34,
+      progressSize: 50,
       progressDirection: 'plus'
     }, options));
   }
@@ -46,21 +50,24 @@ module.exports = class extends Base {
   getProgress(size, value, direction) {
     const k = direction === 'both' ? 0.5 : 1;
     const positive = Math.abs(value);
-    const filled = '█'.repeat(positive * k);
-    const append = '░'.repeat((size - positive) * k);
+
+    const symbol = CHAR_DELIMITER;
+    const filled = CHAR_FILL.repeat(Math.ceil(positive * k));
+    const append = CHAR_EMPTY.repeat(Math.floor((size - positive) * k));
     switch (direction) {
       case 'plus':
-        return filled + append;
+        return symbol + filled + append;
       case 'minus':
-        return append + filled;
+        return append + filled + symbol;
       case 'both':
         if (size % 2 !== 0) {
           throw new Error('You want to build both-directional progress bar, but the size is not even.');
         }
 
-        const empty = '░'.repeat(size * k);
+        const empty = CHAR_EMPTY.repeat(size * k);
         return value > 0
-          ? empty + filled + append : append + filled + empty;
+          ? empty + symbol + filled + append
+          : append + filled + symbol + empty;
       default:
         throw new Error('Only \'plus\', \'minus\' and \'both\' are allowed as \'progressDirection\' property');
     }
